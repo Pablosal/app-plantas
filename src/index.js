9c124fb7894c802fe7190355fb77f58eb9659084
Pablosal <pablosalgadou@gmail.com>
2020-05-26 17:26:41 -0400

    finality

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import "../node_modules/@fortawesome/fontawesome-free/css/all.min.css";
-import "/mnt/c/Users/pablo/Desktop/plantas/node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import { store } from "../src/redux/store/Store";
 import { Provider } from "react-redux";
 ReactDOM.render(
